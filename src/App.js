2f55c94cb3b1a69f222cd7031ddc6de8fb09c4f0
Vathanak <vathanaksol1605@gmail.com>
2024-11-09 18:10:32 +0700

    add components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to <code>NakTech</code> Community
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://naktech.pro"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          naktech.pro
         </a>
       </header>
     </div>
